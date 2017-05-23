b3e22b437cb12e5525cab5f72570230589cd499e
every-sushi <nadia0515@naver.com>
2017-05-24 04:55:57 +0900

    #3확인버튼 돌리기 + 배경색 바꾸기

--- app/src/main/java/rlaskfud0515/kr/hs/emirim/optionmenutest/MainActivity.java
+++ app/src/main/java/rlaskfud0515/kr/hs/emirim/optionmenutest/MainActivity.java
@@ -1,16 +1,24 @@
 package rlaskfud0515.kr.hs.emirim.optionmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linear;
+    Button but;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linear=(LinearLayout) findViewById(R.id.linear);
+        but=(Button) findViewById(R.id.but_ok);
     }
     public boolean onCreateOptionsMenu(Menu menu){
         super.onCreateOptionsMenu(menu);
@@ -18,5 +26,35 @@ public class MainActivity extends AppCompatActivity {
         mInflater.inflate(R.menu.menu1, menu);
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        super.onOptionsItemSelected(item);
+
+        switch(item.getItemId()){
+            case R.id.item_blue:
+                linear.setBackgroundColor(Color.BLUE);
+                return true;
+
+            case R.id.item_pink:
+                linear.setBackgroundColor(Color.RED);
+                return true;
+
+            case R.id.item_green:
+                linear.setBackgroundColor(Color.GREEN);
+                return true;
+
+            case R.id.item_but_rotate:
+                but.setRotation(but.getRotation()+45);
+                return true;
+
+            case R.id.item_but_size:
+                but.setScaleX(but.getScaleX()*2);
+                return true;
+
+
+        }
+        return false;
     }
 }
